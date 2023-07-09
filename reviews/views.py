6202a4f5a6f698a17d823d705679256bdb5806ce
bobdonald <bobdonald@pop-os.localdomain>
2023-07-09 13:42:52 -0600

    update settings + urls + views

--- reviews/views.py
+++ reviews/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Book
+from .utils import average_rating
 def index(request):
-    name = "world"
-    return render(request, "base.html", {"name": name})
+    return render(request, "base.html")
+def book_search(request):
+    search_text = request.GET.get("search", "")
+    return render(request,
+        "reviews/search-results.html",
+        {"search_text": search_text})
